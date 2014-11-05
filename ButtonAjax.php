701639b06664b0063e9dd211d4768e3d47aabc54
Prawee Wongsa <prawee@hotmail.com>
2014-11-05 11:48:40 +0700

    delete Widget and remake to button ajax

--- ButtonAjax.php
+++ ButtonAjax.php
@@ -5,7 +5,7 @@ namespace prawee\widgets;
 /**
  * This is just an example.
  */
-class Widget extends \yii\base\Widget
+class ButtonAjax extends \yii\base\Widget
 {
     public function run()
     {
